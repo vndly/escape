1abee0bd7d200c72fdec86a734d83e7f745fe549
mtogneri <mtogneri@openwt.com>
2018-03-06 22:40:03 +0100

    Adding tiles programatically

--- app/src/main/java/com/mauriciotogneri/escape/activities/MainActivity.java
+++ app/src/main/java/com/mauriciotogneri/escape/activities/MainActivity.java
@@ -4,14 +4,23 @@ import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.mauriciotogneri.androidutils.Screen;
+import com.mauriciotogneri.androidutils.Screen.ScreenSize;
 import com.mauriciotogneri.escape.R;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final int TILES_WIDTH = 24;
+    private static final int TILES_HEIGHT = 14;
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -19,11 +28,41 @@ public class MainActivity extends AppCompatActivity
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
+
+        addTiles();
+    }
+
+    private void addTiles()
+    {
+        Screen screen = new Screen(this);
+        ScreenSize size = screen.size();
+
+        int tileHeight = size.height() / TILES_HEIGHT;
+        int tileWidth = size.width() / TILES_WIDTH;
+        int tileSizePx = Math.min(tileHeight, tileWidth);
+
+        ViewGroup root = findViewById(R.id.container);
+        LayoutInflater inflater = getLayoutInflater();
+
+        for (int i = 0; i < TILES_HEIGHT; i++)
+        {
+            LinearLayout row = new LinearLayout(this);
+
+            for (int j = 0; j < TILES_WIDTH; j++)
+            {
+                TextView tile = (TextView) inflater.inflate(R.layout.tile, row, false);
+                tile.getLayoutParams().width = tileSizePx;
+                tile.getLayoutParams().height = tileSizePx;
+
+                row.addView(tile);
+            }
+
+            root.addView(row);
+        }
     }
 
     private void enableFullScreen(Window window)
     {
-        //window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         if (Build.VERSION.SDK_INT >= VERSION_CODES.KITKAT)
