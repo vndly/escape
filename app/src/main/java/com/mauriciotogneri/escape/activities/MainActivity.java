f8955dc680158ba8cc0c8751e60d796fbf76c257
mtogneri <mtogneri@openwt.com>
2018-03-06 22:02:13 +0100

    Enabled fullscreen

--- app/src/main/java/com/mauriciotogneri/escape/activities/MainActivity.java
+++ app/src/main/java/com/mauriciotogneri/escape/activities/MainActivity.java
@@ -1,7 +1,12 @@
 package com.mauriciotogneri.escape.activities;
 
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.mauriciotogneri.escape.R;
 
@@ -11,6 +16,42 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
+    }
+
+    private void enableFullScreen(Window window)
+    {
+        //window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.KITKAT)
+        {
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+        else if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN)
+        {
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        enableFullScreen(getWindow());
     }
 }
--- app/src/main/java/com/mauriciotogneri/escape/app/Cryptos.java
+++ app/src/main/java/com/mauriciotogneri/escape/app/Cryptos.java
@@ -2,7 +2,7 @@ package com.mauriciotogneri.escape.app;
 
 import android.app.Application;
 
-public class Escape extends Application
+public class Cryptos extends Application
 {
     @Override
     public void onCreate()
